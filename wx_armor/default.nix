b9c61d40b4cbd5e2b1176a3eb17e46a6e488a958
Break Yang <breakds@gmail.com>
2024-01-04 15:44:58 -0800

    Add wx250s config

--- wx_armor/default.nix
+++ wx_armor/default.nix
@@ -27,6 +27,11 @@ stdenv.mkDerivation rec {
     drogon
   ];
 
+  postInstall = ''
+    mkdir -p $out/etc/wx_armor
+    ln -s $src/configs/wx250s_motor_config.yaml $out/etc/wx_armor
+  '';
+
   meta = with lib; {
     description = "Onboard WidowX driver based on Dynamixel SDK";
     homepage = "https://github.com/HorizonRoboticsInternal/interbotix_x";
